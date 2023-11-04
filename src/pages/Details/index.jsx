7e8829c849f31c313f9bcb3b2d597cab42da1afa
rodfrutuoso <rodfrutuoso@hotmail.com>
2023-11-04 08:40:26 -0300

    feito a sessão no details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,23 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 
 export function Details() {
   return (
     <Container>
-      <Header/>
-      <Button title = "Voltar"/>
+      <Header />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="https://github.com/rodfrutuoso" target="_blank">Github @rodfrutuoso</a>
+          </li>
+          <li>
+            <a href="https://github.com/rodfrutuoso" target="_blank">Github @rodfrutuoso</a>
+          </li>
+        </Links>
+      </Section>
+      <Button title="Voltar" />
     </Container>
   )
 }
