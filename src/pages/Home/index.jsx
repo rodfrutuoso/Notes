a7c6dfe880b18ce8081e3119c51cc8903e7e2e9e
rodfrutuoso <rodfrutuoso@hotmail.com>
2023-11-05 21:08:27 -0300

    feito o grit da home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,2 +1,31 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
+import { Header } from "../../components/Header"
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Notes</h1>
+      </Brand>
+      <Header>
+
+      </Header>
+      <Menu>
+
+      </Menu>
+
+      <Search>
+
+      </Search>
+      <Content>
+
+      </Content>
+
+      <NewNote>
+
+      </NewNote>
+
+    </Container>
+  );
+}
