0f79bb59b696ab080734e18a5c0a2f05d955beb1
rodfrutuoso <rodfrutuoso@hotmail.com>
2023-11-08 17:33:04 -0300

    criando página de login

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
 
@@ -11,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
