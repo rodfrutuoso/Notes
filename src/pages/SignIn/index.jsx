aa9731019647c58d0a2c8cb35ab0fd19c317af69
rodfrutuoso <rodfrutuoso@hotmail.com>
2023-11-08 17:52:16 -0300

    finalizada página de login

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,10 +1,31 @@
-import { Container} from "./styles";
-
+import { Container, Form } from "./styles";
+import { Input } from "../../components/Input"
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi"
+import { Button } from "../../components/Button"
 
 export function SignIn() {
   return (
     <Container>
-     
+      <Form>
+        <h1>Notes</h1>
+        <p>Aplicação para gerenciar e salvar notas e links</p>
+        <h2>Faça seu Login</h2>
+        <Input
+          Placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        <Input
+          Placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+        <Button title="Entrar"/>
+
+        <a href="#">
+          Criar Conta
+        </a>
+      </Form>
     </Container>
   );
 }
